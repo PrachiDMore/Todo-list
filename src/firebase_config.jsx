b8c6006306a7ea08735d9a939d54b48d1abf87c7
Prachi More <prachimore2702@gmail.com>
2023-04-10 15:15:58 +0530

    Initial Changes

--- src/firebase_config.jsx
+++ src/firebase_config.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi_h18gY_Tbdcllh-RmCSr3LVDvHrX9AM",
-  authDomain: "todo-f9d3d.firebaseapp.com",
-  projectId: "todo-f9d3d",
-  storageBucket: "todo-f9d3d.appspot.com",
-  messagingSenderId: "144346985029",
-  appId: "1:144346985029:web:8e02097a6ff6a797ffa92c"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 export const app = initializeApp(firebaseConfig);
